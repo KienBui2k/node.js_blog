e243c0d9640ec760fdab2226fbc7f5d1ea29d151
KienBui2k <haikien371316@gmail.com>
2023-07-09 21:59:59 +0700

    router

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const routes = require('./routes/')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
 //HTTP Logger
 app.use(morgan('combined'))
 // node: path / posix
@@ -18,13 +25,10 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// route init
+routes(app);
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
 
-app.get('/news', (req, res) => {
-    res.render('news')
-})
+
 // 127.0.0.1 - localhost 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
